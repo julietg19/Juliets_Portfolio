d92da1821011c1dfddb03f3c28d6687e1110814d
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-08-28 13:43:48 -0500

    nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,12 +4,17 @@ import "./App.css";
 import Nav from "./components/Layouts/Nav";
 import About from "./components/Pages/About";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Home />
-      <About />
+    <div className="App bg-light">
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- portfolio/src/components/Layouts/Nav.js
+++ portfolio/src/components/Layouts/Nav.js
@@ -2,8 +2,52 @@ import React from "react";
 
 export default function Nav() {
   return (
-    <div>
-      <h1>navbar</h1>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="!#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="!#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="!#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="!#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="!#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link disabled"
+                href="!#"
+                tabindex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
